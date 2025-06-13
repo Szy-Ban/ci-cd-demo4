71c3193966908e1ae91efde746f8e3cd13e69315
Szymon B <baniewiczszymonn@gmail.com>
2025-06-13 11:08:53 +0200

    test

--- report-generator.js
+++ report-generator.js
@@ -1,14 +1,27 @@
+// report-generator.js
+
 const fs = require('fs');
 const path = require('path');
 
-const status = process.env.PIPELINE_STATUS || 'unknown';
+// Pobieramy statusy poszczególnych etapów z env
+const statuses = {
+    Testy: process.env.JOB_TEST || 'unknown',
+    Lint: process.env.JOB_LINT || 'unknown',
+    Build: process.env.JOB_BUILD || 'unknown',
+    Staging: process.env.JOB_STAGING || 'unknown',
+    Production: process.env.JOB_PROD || 'unknown'
+};
+
 const branch = process.env.GITHUB_REF || 'unknown';
 const timestamp = new Date().toISOString();
 
-const content = `Status: ${status}\nBranch: ${branch}\nTime: ${timestamp}`;
+let content = `Branch: ${branch}\nCzas: ${timestamp}\n\n`;
+for (const [name, status] of Object.entries(statuses)) {
+    content += `${name.padEnd(11)}: ${status}\n`;
+}
 
 fs.writeFileSync(path.join(__dirname, 'report.txt'), content);
+console.log('Report saved as report.txt:\n');
+console.log(content);
 
-console.log('Report saved as report.txt');
-
-// testtttt
+//test
